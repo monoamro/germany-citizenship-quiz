098e12e510d35ebe77a4659a0269090f7043727e
Montaser Amro <mono.amro@gmail.com>
2023-09-01 22:26:19 +0200

    edits footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
 
 export default function Footer() {
   return (
     <>
       <footer>
-      <p>Created By <b>THE GERMANS</b></p>
-      <p>2023</p>
+        <p>
+          Hergestellt in Deutschland <b>DIGGAA</b>
+        </p>
+        <p>2023</p>
       </footer>
     </>
-    )
+  );
 }
